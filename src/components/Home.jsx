8297745717fb3382bdae6bb6979807e3027cd9c8
jasirmukhtar <mukhtarjasir@gmail.com>
2025-03-30 14:24:38 +0530

    CORS enabled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,31 @@
-function Home(){
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+function Home() {
 
-    return "Hello World"
+    const BASE_URL = 'http://localhost:8080/api/home';
+    const [message, setMessage] = useState('');
+
+    useEffect(()=>{
+
+        axios.get(BASE_URL).
+        then(response => {
+
+            setMessage(response.data)
+
+        }).catch(error => {
+            console.error("Error fetching response")
+        });
+        
+
+    }, [])
+
+
+    
+
+    return <div>
+        <p>{message}</p>
+    </div>
+  
 }
 
 export default Home;
